5c940466ecad005b333ce3e13c6540948d0e8638
Everton-Luiss <elssufpa@gmail.com>
2020-07-14 14:31:11 -0300

    novo

--- Neq.c
+++ Neq.c
@@ -13,7 +13,7 @@ double x, y[N], S, V;
 int j;
 
 output = fopen("numerico.dat", "w");
-out = fopen("analitico.dat", "w");
+//out = fopen("analitico.dat", "w");
 
 printf("Type initial position: ");
 scanf("%lf", &S);
@@ -29,7 +29,7 @@ fprintf(output, "0\t%f\n", y[0]);
 printf("S0 = %g\n", S);
 printf("V0 = %g\n", V);
 
-for (j=1; j*dist<=MAX ;j++)
+for (j=1; j*dist<=MAX; j++)
 {
    x=j*dist;
    runge4(x, y, dist);
@@ -40,7 +40,7 @@ for (j=1; j*dist<=MAX ;j++)
 }
 
 fclose(output);
-fclose(out);
+//fclose(out);
 }
 
 void runge4( double x, double *y, double step)
@@ -48,14 +48,14 @@ void runge4( double x, double *y, double step)
 double c1[N],c2[N],c3[N],c4[N],yy[N],h2=step/2.0;
 int i;
 
-	f(x, y,c1);
-        for (i=0; i<N; i++) yy[i]=y[i]+h2*c1[i];
-        f(x+h2,yy,c2);
-        for (i=0; i<N; i++) yy[i]=y[i]+h2*c2[i];
-        f(x+h2,yy,c3);
-        for (i=0; i<N; i++) yy[i]=y[i]+step*c3[i];
-        f(x+dist,yy,c4);
-        for (i=0; i<N; i++)
-          y[i]+=step*(c1[i]+2.0*c2[i]+2.0*c3[i]+c4[i])/6.0;
+	   f(x, y,c1);
+      for (i=0; i<N; i++) yy[i]=y[i]+h2*c1[i];
+      f(x+h2,yy,c2);
+      for (i=0; i<N; i++) yy[i]=y[i]+h2*c2[i];
+      f(x+h2,yy,c3);
+      for (i=0; i<N; i++) yy[i]=y[i]+step*c3[i];
+      f(x+step,yy,c4);
+      for (i=0; i<N; i++);
+      y[i]+=step*(c1[i]+2.0*c2[i]+2.0*c3[i]+c4[i])/6.0;
 
 }
